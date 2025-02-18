cb9efe512fb49921a8376f8558c95d0f4b005f5f
“joanomana” <joansebastianomasu@gmail.com>
2025-02-18 11:11:11 -0500

    feat: :zap: Segunda prueba contenidos react

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,13 @@ import { FaRegUserCircle} from "react-icons/fa";
 import { IoHeartOutline } from "react-icons/io5";
 import { LuMessageCirclePlus } from "react-icons/lu";
 import { CiSquareMore } from "react-icons/ci";
+import logo from "../storage/img/logos.png";
 
 
 
 const Header = () => (
   <header className="flex justify-between p-2 ">
-    <img src="src/storage/img/logos.png" alt="ig" />
+    <img src={logo} alt="ig" />
     <div className="flex gap-4">
       <IoHeartOutline size={26} />
       <LuMessageCirclePlus size={24} />
